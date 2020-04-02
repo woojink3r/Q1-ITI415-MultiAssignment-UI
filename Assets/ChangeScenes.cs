cf0d0816f0ad9220a518b6fb165188ba64b2842d
Bill Crosbie <crosbie.git@gmail.com>
2020-04-02 13:49:05 -0400

    Initial commit

---
+++ Assets/ChangeScenes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+
+    public string[] sceneNames;
+
+    public void ReturnToMainScene()
+    {
+        SceneManager.LoadScene("scene-MainMenu");
+    }
+
+    public void LoadNewScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName); 
+    }
+
+    public void LoadNewScene1()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+
+}
