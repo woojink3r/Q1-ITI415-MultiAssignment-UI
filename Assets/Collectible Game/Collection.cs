e5059afd8920533c4514b5036b7112f25a2c1187
woojink3r <57843107+woojink3r@users.noreply.github.com>
2020-05-14 20:52:00 -0400

    import and button editted

---
+++ Assets/Collectible Game/Collection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            count = count + 1;
+        }
+    }
+}
